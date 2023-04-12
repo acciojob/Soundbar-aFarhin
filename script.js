02e5e5415ff1380071f6c5590e144121d626339e
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-12 14:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function play() {
     var audio = document.getElementById("audio");
     audio.play();
   }
+
+function stop() {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
+}
